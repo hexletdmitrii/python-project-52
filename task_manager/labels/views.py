35fc228c47c9cbe369e1a7bd9feeb40736f9e0d1
Dmitrii L <hexletdmitrii@gmail.com>
2025-01-06 20:27:28 +0300

    task tests8

--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -8,6 +8,7 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.utils.translation import gettext_lazy as _
 from django.shortcuts import redirect
 from .forms import LabelForm
+from task_manager.tasks.models import Task
 
 class LabelListView(LoginRequiredMixin, ListView):
     model = Label
@@ -45,23 +46,43 @@ class LabelUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
         return context
 
 
-class LabelDeleteView(LoginRequiredMixin, SuccessMessageMixin, UserPassesTestMixin, DeleteView):
+class LabelDeleteView(SuccessMessageMixin, DeleteView):
     model = Label
-    template_name = 'cud/delete.html'
     success_url = reverse_lazy('labels_list')
+    template_name = 'cud/delete.html'
     success_message = _("Метка успешно удалена")
-
-    def test_func(self):
-        label = self.get_object()
-        return not label.tasks.exists()
-
-    def handle_no_permission(self):
-        messages.error(self.request, _("Cannot delete the label as it is linked to tasks!"))
-        return redirect('labels_list')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = _("Удалить метку?")
+        context['title'] = _("Удалить Метку?")
         context['back_url'] = reverse_lazy('labels_list')
-        context['object_del'] = self.get_object().__str__
+        context['object_del'] = self.get_object().__str__()
         return context
+
+    def form_valid(self, form):
+        label = self.get_object()
+        if label.tasks.exists():
+            messages.error(self.request, _("Невозможно удалить метку, так как она связана с задачами!"))
+            return redirect('labels_list')
+        return super().form_valid(form)
+
+# class LabelDeleteView(LoginRequiredMixin, SuccessMessageMixin, UserPassesTestMixin, DeleteView):
+#     model = Label
+#     template_name = 'cud/delete.html'
+#     success_url = reverse_lazy('labels_list')
+#     success_message = _("Метка успешно удалена")
+
+#     def test_func(self):
+#         label = self.get_object()
+#         return not label.tasks.exists()
+
+#     def handle_no_permission(self):
+#         messages.error(self.request, _("Cannot delete the label as it is linked to tasks!"))
+#         return redirect('labels_list')
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['title'] = _("Удалить метку?")
+#         context['back_url'] = reverse_lazy('labels_list')
+#         context['object_del'] = self.get_object().__str__
+#         return context
